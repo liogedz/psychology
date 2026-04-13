db208268fc47defceaf02ffdf7e318b680501196
liogedz <aleksei.gedz@gmail.com>
2026-04-13 10:40:44 +0300

    cleanup of HostListener

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -1,19 +1,20 @@
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavigationEnd, Router, RouterLink} from '@angular/router';
 import {filter} from 'rxjs';
 import {NavbarService} from '@services/navbar-service';
+import {ClickOutside} from '@shared/click-outside';
 
 @Component({
   selector: 'app-header-component',
   imports: [
-    RouterLink
+    RouterLink,
+    ClickOutside
   ],
   templateUrl: './header-component.html',
   styleUrl: './header-component.css',
   standalone: true
 })
 export class HeaderComponent {
-
 
   constructor(
     public navbar: NavbarService,
@@ -24,21 +25,5 @@ export class HeaderComponent {
       .subscribe(() => {
         this.navbar.closeAll();
       });
-  }
-
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: Event) {
-    const target = event.target as HTMLElement;
-
-    if (
-      !target.closest('.mobile-menu') &&
-      !target.closest('.menu-button')
-    ) {
-      this.navbar.isMobileMenuOpen.set(false);
-    }
-
-    if (!target.closest('.services-menu')) {
-      this.navbar.isServicesOpen.set(false);
-    }
   }
 }
